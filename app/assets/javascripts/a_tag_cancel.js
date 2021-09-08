3f77e40dce26f8b951ba51f7466c49909b743ab7
takuma0606 <reol1289@gmail.com>
2021-09-08 16:22:05 +0900

    ハンバーガーメニュー

--- app/assets/javascripts/a_tag_cancel.js
+++ app/assets/javascripts/a_tag_cancel.js
@@ -1,6 +1,13 @@
 $(function (){
-  $('.learned_btn').on('click', function(){
-    $(this).css('color','silver');
+  $('.learned_btn_result').on('click', function(){
+    $(this).html('<i class="fas fa-check-circle fa-lg" style="color: #8EB8FF" ></i>');
+    $(this).css('pointer-events','none');
+  });
+});
+
+$(function (){
+  $('.forgot_btn_result').on('click', function(){
+    $(this).html('<i class="fas fa-check-circle fa-lg" style="color: #C0C0C0"></i>');
     $(this).css('pointer-events','none');
   });
 });
